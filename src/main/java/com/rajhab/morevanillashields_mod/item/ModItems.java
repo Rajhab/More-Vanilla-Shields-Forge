1e7014694c0f176208865d480a43d894afc84181
Rajhab <rajhabcurseforge@gmail.com>
2025-03-16 16:35:48 +0100

    Changelog 1.0.7-1.19.2:
    - Updated to forge 43.5.0
    - Added Copper Shield (325 durability, no function yet)
    - Added Magma Shield (255 durability)
    - Magma Shield burns enemies on hit (5 second burn time, configurable if turned on or off)
    - Removed all tooltips except for the Redstone Shield and End Crystal Shield
    - Reworked Tooltips for End Crystal Shield and Redstone Shield
    - Tooltips now only showing when advanced tooltips are activated (F3 + H)
    - Reduced wear and tear on the shield when the end crystal shield explodes (from 175 -> 65)
    
    COMING SOON:
    - Sculk Shield (emits a sculk signal for sculk sensors (opening doors for example))
    - Copper Shield function with lightning
    - Variants for Wind Burst Shield (knocks back enemies OR very high jumps)

--- src/main/java/com/rajhab/morevanillashields_mod/item/ModItems.java
+++ src/main/java/com/rajhab/morevanillashields_mod/item/ModItems.java
@@ -5,9 +5,11 @@ import com.rajhab.morevanillashields_mod.math.ModVector3d;
 import com.rajhab.morevanillashields_mod.morevanillashields;
 import com.rajhab.morevanillashields_mod.util.ModShieldTileEntityRenderer;
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
 import net.minecraft.core.particles.DustParticleOptions;
+import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.LivingEntity;
@@ -611,6 +613,107 @@ public class ModItems{
                         super.appendHoverText(stack, level, components, flag);
                     }
                 }
+
+                @Override
+                public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+                    consumer.accept(new IClientItemExtensions() {
+                        @Override
+                        public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                            return ModShieldTileEntityRenderer.instance;
+                        }
+                    });
+                }
+
+            });
+
+    public static final RegistryObject<Item> COPPER_SHIELD = ITEMS.register("copper_shield",
+            () -> new ShieldItem(new Item.Properties().defaultDurability(325).tab(CreativeModeTab.TAB_COMBAT)) {
+
+                @Override
+                public int getMaxDamage(ItemStack stack) {
+                    return ShieldConfig.COPPER_SHIELD_DURABILITY.get();
+                }
+
+                @Override
+                public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+                    consumer.accept(new IClientItemExtensions() {
+                        @Override
+                        public BlockEntityWithoutLevelRenderer getCustomRenderer() {
+                            return ModShieldTileEntityRenderer.instance;
+                        }
+                    });
+                }
+
+            });
+
+    public static final RegistryObject<Item> MAGMA_SHIELD = ITEMS.register("magma_shield",
+            () -> new ShieldItem(new Item.Properties().defaultDurability(255).tab(CreativeModeTab.TAB_COMBAT)) {
+
+                @Override
+                public int getMaxDamage(ItemStack stack) {
+                    return ShieldConfig.MAGMA_SHIELD_DURABILITY.get();
+                }
+
+                @Override
+                public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> components, TooltipFlag flag) {
+                    if (ShieldConfig.ENABLE_TOOLTIPS.get() && Minecraft.getInstance().options.advancedItemTooltips) {
+                        if (ShieldConfig.ENABLE_MAGMA_BURN.get()) {
+                            components.add(Component.translatable("item.moditems.magma_shield_burn_enabled").withStyle(ChatFormatting.DARK_AQUA));
+                        }
+                        else {
+                            components.add(Component.translatable("item.moditems.magma_shield_burn_disabled").withStyle(ChatFormatting.DARK_AQUA));
+                        }
+                        super.appendHoverText(stack, level, components, flag);
+                    }
+                }
+
+                @Override
+                public void onUseTick(Level pLevel, LivingEntity pLivingEntity, ItemStack pStack, int pRemainingUseDuration) {
+
+                    if(ShieldConfig.ENABLE_MAGMA_BURN.get()) {
+
+                        if (ShieldConfig.ENABLE_PARTICLES.get()) {
+
+                            float yaw = -pLivingEntity.getYRot();
+                            float pitch = -pLivingEntity.getXRot();
+
+                            double offsetX = 0.5 * Math.sin(Math.toRadians(yaw));
+                            double offsetY = 0.5 * Math.sin(Math.toRadians(pitch));
+                            double offsetZ = 0.5 * Math.cos(Math.toRadians(yaw));
+
+                            ModVector3d offsetVector = new ModVector3d(0.0, 0.0, 0.0);
+
+                            offsetVector.rotateX(-pitch * Math.PI / 180.0);
+                            offsetVector.rotateZ(-yaw * Math.PI / 180.0);
+
+                            offsetX += offsetVector.x;
+                            offsetY += offsetVector.y;
+                            offsetZ += offsetVector.z;
+
+                            Random rand = new Random();
+
+                            if (!pLevel.isClientSide) {
+
+                                ServerLevel pServerLevel = (ServerLevel) pLevel;
+
+                                for (double countparticles = 0; countparticles <= ShieldConfig.MAGMA_SHIELD_DENSITY.get(); ++countparticles) {
+                                    pServerLevel.sendParticles(
+                                            ParticleTypes.SMALL_FLAME,
+                                            (pLivingEntity.position().x + offsetX) + (rand.nextDouble() - 0.5D),
+                                            (pLivingEntity.position().y + offsetY) + (rand.nextDouble() + 0.5D),
+                                            (pLivingEntity.position().z + offsetZ) + (rand.nextDouble() - 0.5D),
+                                            1,
+                                            0.0,
+                                            0.0,
+                                            0.0,
+                                            0.1
+                                    );
+                                }
+                            }
+                        }
+                    }
+                }
+
 
                 @Override
                 public void initializeClient(Consumer<IClientItemExtensions> consumer) {
--- src/main/java/com/rajhab/morevanillashields_mod/util/ModModelPredicateProvider.java
+++ src/main/java/com/rajhab/morevanillashields_mod/util/ModModelPredicateProvider.java
@@ -21,7 +21,7 @@ public class ModModelPredicateProvider {
     @SubscribeEvent
     public static void init(FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
-            addShieldPropertyOverrides(new ResourceLocation(morevanillashields.MOD_ID, "blocking"),
+            addShieldPropertyOverrides(ResourceLocation.fromNamespaceAndPath(morevanillashields.MOD_ID, "blocking"),
                     (stack, world, entity, seed) -> entity != null && entity.isUsingItem() && entity.getUseItem() == stack ? 1.0F : 0.0F,
                     //Shields with Banner Support
                     ModItems.LEATHER_SHIELD.get(),
@@ -31,6 +31,8 @@ public class ModModelPredicateProvider {
                     ModItems.EMERALD_SHIELD.get(),
                     ModItems.AMETHYST_SHIELD.get(),
                     ModItems.REDSTONE_SHIELD.get(),
+                    ModItems.COPPER_SHIELD.get(),
+                    ModItems.MAGMA_SHIELD.get(),
                     ModItems.OBSIDIAN_SHIELD.get(),
                     ModItems.COAL_SHIELD.get(),
                     ModItems.END_CRYSTAL_SHIELD.get(),
@@ -77,6 +79,10 @@ public class ModModelPredicateProvider {
                     LOCATION_AMETHYST_SHIELD_BASE_NOPATTERN,
                     LOCATION_REDSTONE_SHIELD_BASE,
                     LOCATION_REDSTONE_SHIELD_BASE_NOPATTERN,
+                    LOCATION_COPPER_SHIELD_BASE,
+                    LOCATION_COPPER_SHIELD_BASE_NOPATTERN,
+                    LOCATION_MAGMA_SHIELD_BASE,
+                    LOCATION_MAGMA_SHIELD_BASE_NOPATTERN,
                     LOCATION_OBSIDIAN_SHIELD_BASE,
                     LOCATION_OBSIDIAN_SHIELD_BASE_NOPATTERN,
                     LOCATION_COAL_SHIELD_BASE,
@@ -103,6 +109,10 @@ public class ModModelPredicateProvider {
     public static final Material LOCATION_AMETHYST_SHIELD_BASE_NOPATTERN = material("entity/amethyst_shield_base_nopattern");
     public static final Material LOCATION_REDSTONE_SHIELD_BASE = material("entity/redstone_shield_base");
     public static final Material LOCATION_REDSTONE_SHIELD_BASE_NOPATTERN = material("entity/redstone_shield_base_nopattern");
+    public static final Material LOCATION_COPPER_SHIELD_BASE = material("entity/copper_shield_base");
+    public static final Material LOCATION_COPPER_SHIELD_BASE_NOPATTERN = material("entity/copper_shield_base_nopattern");
+    public static final Material LOCATION_MAGMA_SHIELD_BASE = material("entity/magma_shield_base");
+    public static final Material LOCATION_MAGMA_SHIELD_BASE_NOPATTERN = material("entity/magma_shield_base_nopattern");
     public static final Material LOCATION_OBSIDIAN_SHIELD_BASE = material("entity/obsidian_shield_base");
     public static final Material LOCATION_OBSIDIAN_SHIELD_BASE_NOPATTERN = material("entity/obsidian_shield_base_nopattern");
     public static final Material LOCATION_COAL_SHIELD_BASE = material("entity/coal_shield_base");
@@ -113,7 +123,7 @@ public class ModModelPredicateProvider {
     @SuppressWarnings("deprecation")
     private static Material material(String path) {
         return new Material(
-                TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(morevanillashields.MOD_ID, path));
+                TextureAtlas.LOCATION_BLOCKS, ResourceLocation.fromNamespaceAndPath(morevanillashields.MOD_ID, path));
     }
 
 }
